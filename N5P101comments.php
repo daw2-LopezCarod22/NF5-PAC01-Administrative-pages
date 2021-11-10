ad82c6ddf8feeeb9028a00c3b3379e85fca2ec5d
daw2-LopezCarod22 <91276534+daw2-LopezCarod22@users.noreply.github.com>
2021-11-10 17:05:39 +0100

    Add files via upload

---
+++ N5P101comments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>N2P1</title>
+</head>
+<body>
+	<p>Create a list of the 10 most important concepts about what you’ve learned in this document.</p>
+	<ul>
+		<li>He aprendido como funciona un programa que modifica datos de una base de datos mediante botones</li>
+		<li>He aprendido a añadir datos en una base de datos a traves de un programa php anteriormente escrito</li>
+		<li>He aprendido a modificar datos en una base de datos a traves de un programa php anteriormente escrito</li>
+		<li>He aprendido a borrar datos en una base de datos a traves de un programa php anteriormente escrito</li>
+	</ul>
+	<p>Qualifies the document (0-10), and the teacher's explanation (0-10).<br></p>
+	<p>Document: 10<br></p>
+	<p>Teacher explanation: 10<br></p>
+	<p>As a student, put a mark to yourself.<br></p>
+	<p>Myself mark: 10<br></p>
+	<p>Explain an improvement.<br></p>
+	<p>Gracias al croquis he tipo mapa conceptual que hay subido me ha sido muy facil guiarme para hacer la practica, creo que no mejoraria nada.</p>
+</body>
+</html>
